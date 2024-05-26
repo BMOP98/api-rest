4306fe4a55b33e45ba625f0f453ca64f6d6be9a9
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-05-26 01:06:03 -0500

    puerto

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5000;
 
 // Configurar middlewares
 app.use(bodyParser.json());
